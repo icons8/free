8abab2c65fa98cfc9e6931ba1c1d323b21b65c82
Зезегова Ксения Васильевна <fro154pro@outlook.com>
2023-10-16 21:14:26 +0300

    export format and naming scheme

--- src/cs/Free.Schema/Export/ExportFormat.cs
+++ src/cs/Free.Schema/Export/ExportFormat.cs
@@ -1,16 +1,32 @@
 namespace Free.Schema;
 
+/// <summary>
+/// export setting
+/// </summary>
 public class ExportFormat
 {
+    /// <summary>
+    /// todo
+    /// </summary>
     public float Size { get; set; }
-
+    /// <summary>
+    /// export format, default is png
+    /// </summary>
     public string Format { get; set; } = "png";
-
+    /// <summary>
+    /// suffix/preffix (text), default is empty
+    /// </summary>
     public string Name { get; set; } = "";
-
+    /// <summary>
+    /// is suffix or preffix, default is suffix
+    /// </summary>
     public NamingScheme Naming { get; set; } = NamingScheme.Suffix;
-
+    /// <summary>
+    /// scale with which the object is imported
+    /// </summary>
     public float Scale { get; set; }
-
+    /// <summary>
+    /// scale(px)/width/height, default is scale, right now we're only supporting the scale
+    /// </summary>
     public ScaleType Type { get; set; } = ScaleType.Scale;
 }
--- src/cs/Free.Schema/Export/NamingScheme.cs
+++ src/cs/Free.Schema/Export/NamingScheme.cs
@@ -1,8 +1,15 @@
 namespace Free.Schema;
 
+/// <summary>
+/// scheme for names of imported objects (suffix/preffix)
+/// </summary>
+[SketchCompatibility]
 public enum NamingScheme : byte
 {
     Suffix = 0,
+    /// <summary>
+    /// we use secondary and primary prefix as a prefix
+    /// </summary>
     SecondaryPrefix = 1,
     PrimaryPrefix = 2,
 }
