b28df0dd4fe6f2b1827f0651bfe6558892eba886
Зезегова Ксения Васильевна <fro154pro@outlook.com>
2023-10-16 20:43:55 +0300

    Shared styles

--- src/cs/Free.Schema/Document/SharedStyle.cs
+++ src/cs/Free.Schema/Document/SharedStyle.cs
@@ -1,9 +1,18 @@
 namespace Free.Schema.Document;
 
+/// <summary>
+/// style saved in the document
+/// </summary>
 public class SharedStyle
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    /// <summary>
+    /// Layer style settings
+    /// </summary>
     public Style Style { get; }
+    /// <summary>
+    /// If this is a text style, text settings will be available
+    /// </summary>
     public TextStyle? TextStyle { get; set; }
 }
