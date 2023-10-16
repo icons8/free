ccba4936e2d09e234c2c34b334ebfbdf6d05f5b6
Sergey Mikolaitis <blasterloremaster@gmail.com>
2023-10-16 20:05:40 +0300

    Remove color parsing logic

--- src/cs/Free.Schema/BoolOp.cs
+++ src/cs/Free.Schema/BoolOp.cs
@@ -1,10 +1,28 @@
 namespace Free.Schema;
 
+/// <summary>
+/// The type of operation used to combine the children of this node.
+/// </summary>
 public enum BoolOp : sbyte
 {
+    /// <summary>
+    /// Not set. Works like Difference or like Union depends on context
+    /// </summary>
     None = -1,
+    /// <summary>
+    /// Combine
+    /// </summary>
     Union = 0,
+    /// <summary>
+    /// Subtract one from another
+    /// </summary>
     Subtract = 1,
+    /// <summary>
+    /// Get shapes intersection
+    /// </summary>
     Intersect = 2,
+    /// <summary>
+    /// Draw shapes difference
+    /// </summary>
     Difference = 3,
 }
