3d921283e4354b17eb9a476002e298bb22b01844
Sergey Mikolaitis <blasterloremaster@gmail.com>
2023-10-18 00:27:22 +0300

    add some comments

--- src/cs/Free.Schema/Layers/Group.cs
+++ src/cs/Free.Schema/Layers/Group.cs
@@ -13,8 +13,14 @@ public class Group : Styled
     /// When enabled, users can select group items without first selecting the group.
     /// </summary>
     public bool SkipSelect { get; set; }
+    /// <summary>
+    /// This is for constraints compatibility with sketch. If file is imported from sketch format it should be false to handle constraints properly.
+    /// </summary>
     [SketchCompatibility]
     public bool SkipConstraints { get; set; }
+    /// <summary>
+    /// Sketch Group Layout. We don't support it but keep it for compatibility
+    /// </summary>
     [SketchCompatibility]
     public IGroupLayout? GroupLayout { get; set; }
 
--- src/cs/Free.Schema/Layers/Shape.cs
+++ src/cs/Free.Schema/Layers/Shape.cs
@@ -4,8 +4,14 @@ public class Shape : Styled
 {
     public override string _t => "SHAPE";
     
+    /// <summary>
+    /// This is for constraints compatibility with sketch. If file is imported from sketch format it should be false to handle constraints properly.
+    /// </summary>
     [SketchCompatibility]
     public bool SkipConstraints { get; set; }
+    /// <summary>
+    /// Sketch Group Layout. We don't support it but keep it for compatibility
+    /// </summary>
     [SketchCompatibility]
     public IGroupLayout? GroupLayout { get; set; }
     public List<Layer> Layers { get; } = new();
