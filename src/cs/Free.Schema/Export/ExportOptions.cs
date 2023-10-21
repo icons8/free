8ec5db997fa92eac3616bab37aababc7e0e4dfa1
uksla <alsku2016@yandex.ru>
2023-10-21 13:59:11 +0300

    ExportOptions description ready

--- src/cs/Free.Schema/Export/ExportOptions.cs
+++ src/cs/Free.Schema/Export/ExportOptions.cs
@@ -1,11 +1,17 @@
 namespace Free.Schema;
 
+/// <summary>
+/// Export options.
+/// </summary>
 public class ExportOptions
 {
+    /// <summary>
+    /// List of export formats set by the user.
+    /// </summary>
     public List<ExportFormat> Formats { get; set; } = new();
     
     /// <summary>
-    /// trim empty pixels for exported image
+    /// When enabled, trims empty pixels in exported images.
     /// </summary>
     public bool Trim { get; set; }
 }
