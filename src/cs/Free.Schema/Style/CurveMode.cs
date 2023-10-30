c7825aeb8a4e8db27072744d982f047dab03ece2
uksla <alsku2016@yandex.ru>
2023-10-30 22:10:23 +0300

    Fill and other - descriptions added

--- src/cs/Free.Schema/Style/CurveMode.cs
+++ src/cs/Free.Schema/Style/CurveMode.cs
@@ -1,10 +1,27 @@
 namespace Free.Schema;
-
+/// <summary>
+/// Defines the types of points on Bézier curves.
+/// </summary>
 public enum CurveMode : byte
 {
+    /// <summary>
+    /// undefined.
+    /// </summary>
     None = 0,
+    /// <summary>
+    /// Straight.
+    /// </summary>
     Straight = 1,
+    /// <summary>
+    /// Mirrored.
+    /// </summary>
     Mirrored = 2,
+    /// <summary>
+    /// Asymmetric.
+    /// </summary>
     Asymmetric = 3,
+    /// <summary>
+    /// Disconnected.
+    /// </summary>
     Disconnected = 4,
 }
--- src/cs/Free.Schema/Style/Fill.cs
+++ src/cs/Free.Schema/Style/Fill.cs
@@ -1,17 +1,40 @@
 namespace Free.Schema;
+/// <summary>
+/// Defines the fill applied to a layer.
+/// </summary>
 
 public class Fill
 {
+    /// <summary>
+    /// If a fill is added.
+    /// </summary>
     public bool Enabled { get; set; }
+    /// <summary>
+    /// Fill color.
+    /// </summary>
     public Color Color { get; set; }
     /// <summary>
-    /// Color Variable ID
+    /// Color variable ID.
     /// </summary>
     public Guid? ColorId { get; set; }
+    /// <summary>
+    /// Defines the fill type.
+    /// </summary>
     public FillType Type { get; set; }
+    /// <summary>
+    /// Defines the fill opacity.
+    /// </summary>
     public float Opacity { get; set; }
+    /// <summary>
+    /// Defines the blend mode.
+    /// </summary>
     public BlendMode BlendMode { get; set; } 
-
+    /// <summary>
+    /// Defines the pattern for image fills: tile, stretch, etc.
+    /// </summary>
     public Pattern Pattern { get; } = new();
+    /// <summary>
+    /// If the fill is a gradient. ????
+    /// </summary>
     public Gradient Gradient { get; } = new();
 }
